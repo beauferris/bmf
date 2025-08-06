db0096a2ef74a203dd3ddd59be6fb5450ef1e0a3
beauferris <h-beauferris@hotmail.com>
2025-08-06 11:47:20 -0600

    robots

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,17 +1,6 @@
-const pluginSitemap = require("@quasibit/eleventy-plugin-sitemap");
-const site = require("./src/_data/site"); // assume site.json exports { url: "https://yoursite.com"
+// const pluginSitemap = require("@quasibit/eleventy-plugin-sitemap");
+
 module.exports = function (eleventyConfig) {
-  // eleventyConfig.addPassthroughCopy({
-  //   "node_modules/govuk-frontend/dist/govuk/all.js": "assets/js/govuk.js",
-  // });
-
-  eleventyConfig.addPlugin(pluginSitemap, {
-    sitemap: {
-      hostname: "https://www.bmftowing.ca/",
-      lastmodProperty: "date",
-    },
-  });
-
   eleventyConfig.addPassthroughCopy({ "src/scripts": "scripts" });
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("dist");
